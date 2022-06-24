7bf05a503b284e2e7f04b29da4a5138db50e7846
nicoseng <nicolas.sengmany@gmail.com>
2022-06-24 16:10:44 +0200

    maj

--- purbeurre_website/product_injector_in_table.py
+++ purbeurre_website/product_injector_in_table.py
@@ -28,8 +28,8 @@ class ProductInjectorInTable:
                     )
                     product_data.save()
 
-            for product in product_table.reverse():
-                if Product.objects.filter(product_name=product.product_name).count() > 1:
-                    product.delete()
+            for row in product_table.reverse():
+                if Product.objects.filter(product_name=row.product_name).count() > 1:
+                    row.delete()
 
         return product_table
