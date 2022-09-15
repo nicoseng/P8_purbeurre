55de081c85c73cf379a3d8b85eddd16a464d4248
nicoseng <nicolas.sengmany@gmail.com>
2022-09-15 11:33:06 +0200

    ajout de la base de données postgresql

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purbeurre_db',
+        'USER': 'postgres',
+        'PASSWORD': 'a8ln17',
+        'PORT': '5432',
     }
 }
 
