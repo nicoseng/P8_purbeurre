1904ef55293946e4797b0a87deabe76e9c423d24
nicoseng <nicolas.sengmany@gmail.com>
2022-09-16 15:49:12 +0200

    test heroku

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'purbeurre_db',
-#         'USER': 'PostgreSQL 14',
-#         'PASSWORD': 'a8ln17',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purbeurre_db',
+        'USER': 'PostgreSQL 14',
+        'PASSWORD': 'a8ln17',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
